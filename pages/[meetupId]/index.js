13ac94dc1dc166af68f82fa3e03c73ffaeed06ed
Bhavya Giri <bhavya.giri44@gmail.com>
2022-09-14 12:58:50 +0530

    new component

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,13 +1,10 @@
 import React, { Fragment } from 'react'
+import MeetupDetail from '../../components/meetups/MeetupDetail'
 
 const MeetupDetails = () => {
   return (
-    <Fragment>
-        <img src='https://blog.close.com/content/images/hubfs/315483/customer-meetup.jpeg' alt='1st'/>
-        <h1>1st meetup</h1>
-        <address> Delhi</address>
-        <p>some description</p>
-    </Fragment>
+        <MeetupDetail image= 'https://blog.close.com/content/images/hubfs/315483/customer-meetup.jpeg'
+        title = '1st' address ='Delhi' description = 'something somthing'/>
   )
 }
 
