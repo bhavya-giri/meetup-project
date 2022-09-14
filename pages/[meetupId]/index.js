44bbf601e58db195d47542daa5c3b34d0d2f727f
Bhavya Giri <bhavya.giri44@gmail.com>
2022-09-14 12:52:20 +0530

    meetup details harcoded

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
-const MeetupId = () => {
+const MeetupDetails = () => {
   return (
-    <div>MeetupId</div>
+    <Fragment>
+        <img src='https://blog.close.com/content/images/hubfs/315483/customer-meetup.jpeg' alt='1st'/>
+        <h1>1st meetup</h1>
+        <address> Delhi</address>
+        <p>some description</p>
+    </Fragment>
   )
 }
 
-export default MeetupId
+export default MeetupDetails
