e8b91da2ab66ba66942bfcd8b5d5c8fe0a57d8cc
Bhavya Giri <bhavya.giri44@gmail.com>
2022-09-14 15:44:46 +0530

    https

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import NewMeetupForm from '../../components/meetups/NewMeetupForm'
 const NewMeetup = () => {
-    const handleAddMeetup =(meetupDetails)=>{
-        console.log(meetupDetails)
+    async function handleAddMeetup(enteredMeetupData){
+      const response = await fetch ('/api/new-meetup',{
+        method: 'POST',
+        body: JSON.stringify(enteredMeetupData),
+        headers:{
+          'Content-Type' :'application/json'
+        }
+      })
+      const data = await response.json()
+      console.log(data)
     }
   return (
     <NewMeetupForm onAddMeetup={handleAddMeetup}/>
